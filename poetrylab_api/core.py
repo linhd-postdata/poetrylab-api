a6b0826fd802010e05677a30ded43be63f5b0f4f
Javier de la Rosa <versae@gmail.com>
2020-02-05 11:28:44 +0100

    Adding caching of pipelines between calls

--- poetrylab_api/core.py
+++ poetrylab_api/core.py
@@ -2,7 +2,12 @@
 import io
 import traceback
 from jollyjumper.core import get_enjambment
+from jollyjumper.pipeline import load_pipeline as enjambment_pipeline
 from rantanplan.core import get_scansion
+from rantanplan.pipeline import load_pipeline as scansion_pipeline
+
+# load_pipeline should work as a "singleton"
+_load_pipeline = {}
 
 
 def get_analysis(poem, operations):
@@ -17,10 +22,15 @@ def get_analysis(poem, operations):
     poem = poem.decode('utf-8')
     output = {}
     for operation in operations:
+        # Caching pipelines between calls
+        if operation not in _load_pipeline:
+            pipeline = locals().get(f"{operation}_pipeline")
+            _load_pipeline[operation] = pipeline() if pipeline else lambda x: x
+        poem_doc = _load_pipeline[operation](poem)
         if operation == "scansion":
-            output[operation] = get_traceback(get_scansion, poem)
+            output[operation] = get_traceback(get_scansion, poem_doc)
         if operation == "enjambment":
-            output[operation] = get_traceback(get_enjambment, poem)
+            output[operation] = get_traceback(get_enjambment, poem_doc)
     return output
 
 
